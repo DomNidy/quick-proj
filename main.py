3014fbf677acaeccbe1ae1784811e00cb8df20e1
DomNidy <dominicnidy3@gmail.com>
2025-04-14 10:40:36 -0400

    Ported over dataclass to be Pydantic BaseModels

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import subprocess
 import shutil
 from pathlib import Path
 from dataclasses import dataclass, field
-from dataclasses_json import dataclass_json, DataClassJsonMixin, Undefined
-from typing import List, Any
+from dataclasses_json import dataclass_json, Undefined
+from typing import List, Union, Optional, Annotated
 from utils import generate_project_title
+from pydantic import BaseModel, Field, field_validator, ValidationError
 import os
+import shutil
 
 
 def is_windows() -> bool:
@@ -23,134 +25,101 @@ class ProjectTemplateNotExists(Exception):
     pass
 
 
-@dataclass_json(undefined=Undefined.RAISE)
-@dataclass
-class EditorOptions:
+class EditorOptions(BaseModel):
     """Specifies the command that should be used to open a text editor and any arguments that should be passed along"""
 
-    command: List[str] = field(default_factory=lambda: ["code"])
-
-
-@dataclass_json(undefined=Undefined.RAISE)
-@dataclass
-class ProjectTemplate:
-    """A project template defines the sequence of commands that are used to instantiate a project"""
-
-    template_name: str
-    editor_override: EditorOptions = field(default_factory=lambda: None)
-    init_steps: List[List[str]] = field(default_factory=lambda: [])
-
-
-@dataclass
-class Config(DataClassJsonMixin):
-    base_project_instantiation_directory: str = field(
-        default_factory=lambda: str(Path.home() / "quick-projects")
+    command: List[str] = Field(
+        description="Command to open the text editor of your choice", examples=["code"]
     )
-    templates: List[ProjectTemplate] = field(
-        default_factory=lambda: [
-            ProjectTemplate(
-                "Python",
-                init_steps=[
-                    (["cmd", "/c"] if is_windows() else []) + ["echo", "Hello!"]
-                ],
-            )
-        ]
+
+
+class ProjectTemplate(BaseModel):
+    """A project template defines the sequence of commands that are used to instantiate a project."""
+
+    name: Annotated[
+        str,
+        Field(
+            description="Name of the template. Should be concise, but provide enough detail.",
+            examples=["Python (Torch CUDA and UV)", "C++ (spdlog, GTest, CMake)"],
+        ),
+    ]
+    description: Annotated[
+        Optional[str],
+        Field(
+            description="A description for the project template. Optional.",
+            examples=[
+                "This template creates a Python project using the UV package manager. It also installs torch (with CUDA support), then creates a Python notebook file that imports it."
+            ],
+        ),
+    ]
+    editor_override: Annotated[
+        Optional[EditorOptions],
+        Field(
+            description="Editor to use when instantiating this project template. Optional.",
+            default=None,
+        ),
+    ]
+    init_steps: Annotated[
+        List[List[str]],
+        Field(
+            description="The sequence of commands to execute while instantiating the template. These commands will be executed with the current working directory being set to newly created project's folder"
+        ),
+    ]
+
+
+class Config(BaseModel):
+    instantiation_directory: str = Field(
+        description="Directory that projects will be created at",
     )
-    editor: EditorOptions = field(default_factory=EditorOptions)
+    templates: List[ProjectTemplate] = Field(
+        description="List of ProjectTemplates",
+    )
+    editor: EditorOptions = Field(description="The default text editor to use")
 
-    def __post_init__(self):
-        # Create .quick-proj if it doesn't exist
-        quick_proj_dir = Path.home() / ".quick-proj"
-        if not quick_proj_dir.exists():
-            quick_proj_dir.mkdir()
-            print(f"Created quick-proj directory at: {quick_proj_dir}")
-
-        # Check if config file is inside quick-proj dir
-        config_path = quick_proj_dir / "config.json"
-        if not config_path.exists():
-            print(
-                'Config file "config.json" does not exist in quick-proj directory, creating one'
-            )
-            config_path.touch()
-            # Serialize the Config object's fields to the config file
-            with open(config_path, "w+") as f:
-                f.write(self.to_json(indent=4))
-        else:
-            with open(config_path, "r") as f:
-                try:
-                    existing_config = json.load(
-                        f, object_hook=Config._decode_dict_to_class_object
-                    )
-                except json.JSONDecodeError as e:
-                    raise InvalidConfig(
-                        f"Tried to load config file, but it was not valid JSON: {e}"
-                    )
-            for k, v in existing_config.items():
-                if not hasattr(self, k):
-                    raise InvalidConfig(
-                        f'Config file contained unexpected field: "{k}"'
-                    )
-                self.__setattr__(k, v)
-
-        self._validate_config()
-
-    def _decode_dict_to_class_object(obj: dict[Any, Any]) -> Any:
-        """Performs pattern matching on an object and tries to match it to one of the
-        classes that the Config object needs.
-
-        Args:
-            obj (dict[Any, Any]): Object to attempt decoding
-
-        Raises:
-            InvalidConfigException: If the object does not match any of the config-related dataclasses
+    @staticmethod
+    def _get_default_config() -> "Config":
+        """Returns a Config object with reasonable defaults
 
         Returns:
-            Any: A class instance. The type of this object can be any of the config-related dataclasses (e.g., `EditorOptions`)
+            Config: _description_
         """
-        match obj:
-            case {"command": c} if isinstance(c, list):
-                return EditorOptions(**obj)
-            case {
-                "base_project_instantiation_directory": _,
-                "templates": _,
-                "editor": _,
-            }:
-                return obj
-            case {
-                "template_name": tn,
-                "editor_override": eo,
-                "init_steps": ins,
-            } if (
-                isinstance(tn, str)
-                and isinstance(ins, list)
-                and (isinstance(eo, type(None)) or isinstance(eo, EditorOptions))
-            ):
-                return ProjectTemplate(**obj)
-            case _:
-                raise InvalidConfig(
-                    f"Failed to decode a nested object that was in the config file: \n{json.dumps(obj, indent=4)}\n"
-                    "Ensure that you defined all fields in the config object, and that their\n"
-                    "types match the types defined in the corresponding Python dataclass. This\n"
-                    "usually happens when you only defined a subset of properties, or one\n"
-                    "property's type does not match the intended Python type."
+        return Config(
+            instantiation_directory=str(Path.home() / "quick-projects"),
+            editor=EditorOptions(command=["code"]),
+            templates=[
+                ProjectTemplate(
+                    name="Python Application (UV)",
+                    description="Creates a Python project using the UV package manager. The created project is configured to be built as an application, rather than a library.",
+                    init_steps=[
+                        [
+                            "uv",
+                            "init",
+                            "--app",
+                            "--name",
+                            "my-project",
+                            "--author-from",
+                            "auto",
+                            "--vcs",
+                            "git",
+                        ]
+                    ],
                 )
-
-    def _validate_config(self):
-        base_project_instantiation_directory = Path(
-            self.base_project_instantiation_directory
+            ],
         )
-        if not base_project_instantiation_directory.exists():
-            print(
-                f'base_project_instantiation_directory "{base_project_instantiation_directory}" does not exist, creating it'
+
+    @field_validator("instantiation_directory", mode="after")
+    @classmethod
+    def _check_instantiation_directory_exists(cls, instantiation_directory: str) -> str:
+        if not Path(instantiation_directory).exists():
+            raise ValueError(
+                f"Instantiation directory '{instantiation_directory}' does not exist"
             )
-            base_project_instantiation_directory.mkdir()
+        return instantiation_directory
 
 
 class QuickProject:
     def __init__(self, config: Config) -> None:
-        self.base_project_instantiation_directory = (
-            config.base_project_instantiation_directory
-        )
+        self.instantiation_directory_path = Path(config.instantiation_directory)
         self.templates = config.templates
         self.editor = config.editor
 
@@ -162,7 +131,7 @@ class QuickProject:
         """
         pt = None
         for template in self.templates:
-            if template_name == template.template_name:
+            if template_name == template.name:
                 pt = template
                 break
 
@@ -170,33 +139,36 @@ class QuickProject:
             raise ProjectTemplateNotExists(
                 f'Project template "{template_name}" does not exist, check your config.\n'
                 "Valid template names are: "
-                + ",".join(f'"{pt.template_name}"' for pt in self.templates)
+                + ",".join(f'"{pt.name}"' for pt in self.templates)
             )
 
-        print(f"Got template: {pt.to_json(indent=4)}")
-
-        base_project_instantiation_directory = Path(
-            self.base_project_instantiation_directory
-        )
+        print(f"Got template: {pt.model_dump_json(indent=4)}")
 
         # If no project_name was provided, construct one
         if project_name is None:
             project_name = generate_project_title()
-            while (base_project_instantiation_directory / project_name).exists():
-                continue
-
-        project_root = base_project_instantiation_directory / project_name
+            attempts, max_attempts = 0, 1000
+            while (
+                self.instantiation_directory_path / project_name
+            ).exists() and attempts < max_attempts:
+                attempts += 1
+            else:
+                project_root_path = self.instantiation_directory_path / project_name
+        else:
+            project_root_path = self.instantiation_directory_path / project_name
 
         # Create folder to store project
         try:
-            project_root.mkdir()
+            project_root_path.mkdir()
         except FileExistsError as e:
             e.add_note(
                 f'This occured during instantiation of project template: "{pt.template_name}"'
             )
             raise e
 
-        print(f"Created project folder at: {project_root}")
+        print(f"Created project folder at: {project_root_path}")
+
+        # Now, execute all commands in the template
         try:
             for i, step in enumerate(pt.init_steps):
                 print(f"Step {i+1}: {step}")
@@ -209,11 +181,14 @@ class QuickProject:
                     command.extend(step[1:])
 
                 print(f"\t > {command}")
-                subprocess.run(command, cwd=project_root)
+                subprocess.run(command, cwd=project_root_path)
         except Exception as e:
             print(f"Error occured while instantiating the project: {e}")
-            print(f'Removing the project dir "{project_root}" as its useless now...')
-            project_root.rmdir()
+            print(
+                f'Removing the project dir "{project_root_path}" as its useless now...'
+            )
+
+            project_root_path.rmdir()
 
     def open_editor(self):
         """Opens the editor. The editor is specified in the config file."""
@@ -222,9 +197,49 @@ class QuickProject:
         subprocess.run(editor_executable_path, shell=True)
 
 
-c = Config()
+def load_config(config_path: Optional[Union[Path, str]] = None) -> "Config":
+    """Load a config file from disk.
+
+    If `config_path` is not provided, then we will attempt to
+    load it from the default location in the user's home directory.
+    If the config does not exist within the user's home directory,
+    it will be created.
+
+    Args:
+        config_path (Optional[Union[Path, str]]): Path to the config file
+
+    Raises:
+        InvalidConfig: If any validation errors occur
+
+    Returns:
+        Config: The deserialized config object
+    """
+    if isinstance(config_path, str):
+        config_path = Path(config_path)
+    elif config_path is None:
+        config_directory_path = Path.home() / ".quick-proj"
+        if not config_directory_path.exists():
+            config_directory_path.mkdir()
+            print(
+                f"Default quick-proj directory did not exist, created it at: {config_directory_path}"
+            )
+        config_path = config_directory_path / "config.json"
+
+        if not config_path.exists():
+            # Create new config.json file with default configuration
+            with open(config_path, "w+") as f:
+                f.write(Config._get_default_config().model_dump_json(indent=4))
+            print(f"Created default config.json file at: {config_path}")
+
+    if not config_path.exists():
+        raise FileNotFoundError(f"Config file {config_path} does not exist")
+
+    return Config.model_validate_json(config_path.read_text(), strict=True)
+
+
+c = load_config()
 app = QuickProject(c)
-app.instantiate_project("Python")
+app.instantiate_project("Python Application (UV)")
 
 
 def main():
